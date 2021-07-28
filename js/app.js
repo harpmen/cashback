a04c8d0b615ab70744788a59f1fb14b5a86f64cc
Harpmen <harpvad@gmail.com>
2021-07-28 12:11:46 +0300

    calculator simple working version

--- js/app.js
+++ js/app.js
@@ -16,19 +16,38 @@ function calculateCashback(specialCategoryPurchases, otherCategoryPurchases) {
 }
 const cashback = calculateCashback(5000, 10000);
 console.log(cashback);
-//function handleClick(evt) {
-//    debugger;
-//}
-
-//const formEl = document.getElementById('cashback-form');
-//formEl.onclick = handleClick;
 function handleSubmit(evt) {
     evt.preventDefault();
 
     const specialAmountInputEl = document.getElementById('special-amount-input');
-    const specialAmount = specialAmountInputEl.value;
+    const specialAmount = Number(specialAmountInputEl.value);
+    if (Number.isNaN(specialAmount)) {
+        // TODO: show error
+        return;
+    }
+    if (!Number.isFinite(specialAmount)) {
+        // TODO: show error
+        return;
+    }
+
     const otherAmountInputEl = document.getElementById('other-amount-input');
-    const otherAmount = otherAmountInputEl.value;
+    const otherAmount = Number(otherAmountInputEl.value);
+    if (Number.isNaN(otherAmount)) {
+        // TODO: show error
+        return;
+    }
+    if (!Number.isFinite(otherAmount)) {
+        // TODO: show error
+        return;
+    }
+
+    const result = calculateCashback(specialAmount, otherAmount);
+    const specialCashbackEl = document.getElementById('special-cashback');
+    specialCashbackEl.textContent = `${result.specialCategoryCashback} руб.`;
+    const otherCashbackEl = document.getElementById('other-cashback');
+    otherCashbackEl.textContent = `${result.otherCategoryCashback} руб.`;
+    const totalCashbackEl = document.getElementById('total-cashback');
+    totalCashbackEl.textContent = `${result.totalCashback} руб.`;
 }
 
 const formEl = document.getElementById('cashback-form');
